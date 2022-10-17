589040d76b968f96d1500a7a848ea8d643f8120c
onetayjones <taylordjone@gmail.com>
2022-10-17 13:03:20 +0200

    rendering menu via JS

--- data.js
+++ data.js
@@ -4,20 +4,23 @@ export const menuArray = [
         ingredients: ["pepperoni", "mushrom", "mozarella"],
         id: 0,
         price: 14,
-        emoji: "🍕"
+        emoji: "🍕",
+        image: 'images/pizza.png'
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
         price: 12,
         emoji: "🍔",
-        id: 1
+        id: 1,
+        image: 'images/hamburger.png'
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
         emoji: "🍺",
-        id: 2
+        id: 2,
+        image: 'images/beer.png'
     }
 ]
--- script.js
+++ script.js
@@ -1,2 +1,33 @@
-import { menuArray } from "./data";
+import { menuArray } from "./data.js";
 
+function getMenuHtml(){
+    let menuHtml = ``
+
+    menuArray.forEach(function(item){
+        menuHtml += `
+                <div class="menu-items">
+                    <div class="menu-img">
+                        <img class="menu-icons" src="${item.image}" alt="pizza icon">
+                    </div>
+
+                    <div class="menu-text">
+                        <h3>${item.name}</h3>
+                        <h6>${item.ingredients}</h6>
+                        <h5>$${item.price}</h5>
+                    </div>
+                    
+                    <button class="menu-btn" id="menu-btn">
+                        <i class="fa-regular fa-plus fa-xl"></i>
+                    </button>
+                </div>
+    `
+    })
+    return menuHtml
+}
+
+function render(){
+    document.getElementById('menu-list').innerHTML = getMenuHtml()
+    
+}
+
+render()
