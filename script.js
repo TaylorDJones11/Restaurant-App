598ec96b4baac982eee07baed1a2f5990d358260
onetayjones <taylordjone@gmail.com>
2022-10-19 18:19:24 +0200

    add menu items functionality

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
 import { menuArray } from "./data.js";
 
+
+// Click each menu item
+document.addEventListener('click', function(e){
+    if(e.target.dataset.add){
+        console.log(e.target.dataset.add)
+       getOrdertHtml(e.target.dataset.add)
+    }
+})
+
+function getOrdertHtml(){
+    let orderHtml = ''
+
+    document.getElementById('order').classList.toggle('hidden')
+
+    menuArray.forEach(function(food){
+        orderHtml += `
+       
+            <h3 class="order-text">Your order</h3>
+            <div class="order-list">
+                <h3 class="order-item">${food.name}</h3>
+                <h6 class="remove-item" id="remove-item">remove</h6>
+                <h5 class="order-price">${food.price}</h5>
+            </div>
+            <div class="total-price" id="total-price">
+                <h3 class="price-text">Total price:</h3>
+                <h5 class="total-price-text" id="total-price-text">$26</h5>
+            </div>
+            <button class="complete-btn" id="complete-btn">Complete Order</button>
+        
+       
+    `
+    })
+    
+}
+
+
 function getMenuHtml(){
     let menuHtml = ``
 
     menuArray.forEach(function(item){
+
+
         menuHtml += `
                 <div class="menu-items">
                     <div class="menu-img">
@@ -16,14 +54,19 @@ function getMenuHtml(){
                         <h5>$${item.price}</h5>
                     </div>
                     
-                    <button class="menu-btn" id="menu-btn">
+                    <button class="menu-btn" id="menu-btn" data-add="${item.id}">
                         <i class="fa-regular fa-plus fa-xl"></i>
                     </button>
+                </div>
+                <div class="hidden" id="order-${item.id}">
+                    
                 </div>
     `
     })
     return menuHtml
 }
+
+
 
 function render(){
     document.getElementById('menu-list').innerHTML = getMenuHtml()
