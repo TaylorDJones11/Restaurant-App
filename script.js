88c4e9b42491ed8e02184c1f93d970e5143307af
onetayjones <taylordjone@gmail.com>
2022-11-13 15:28:28 +0200

    displaying the prices in order

--- script.js
+++ script.js
@@ -6,36 +6,54 @@ document.addEventListener('click', function(e){
     if(e.target.dataset.add){
         console.log(e.target.dataset.add)
        getOrdertHtml(e.target.dataset.add)
+       addMenuItem(e.target.dataset.add)
     }
 })
 
-function getOrdertHtml(){
-    let orderHtml = ''
+let orderItem = document.getElementById('order')
 
-    document.getElementById('order').classList.toggle('hidden')
 
-    menuArray.forEach(function(food){
-        orderHtml += `
-       
-            <h3 class="order-text">Your order</h3>
-            <div class="order-list">
-                <h3 class="order-item">${food.name}</h3>
-                <h6 class="remove-item" id="remove-item">remove</h6>
-                <h5 class="order-price">${food.price}</h5>
-            </div>
-            <div class="total-price" id="total-price">
-                <h3 class="price-text">Total price:</h3>
-                <h5 class="total-price-text" id="total-price-text">$26</h5>
-            </div>
-            <button class="complete-btn" id="complete-btn">Complete Order</button>
-        
-       
-    `
-    })
+function addMenuItem(){
+    orderItem.addEventListener("click", )
+    orderItem.removeEventListener('click', addMenuItem)
     
 }
 
 
+
+
+// updates the order with pricing 
+function getOrdertHtml(){
+
+    let orderHtml = ''
+    
+    menuArray.forEach(function(food){
+        orderHtml += `
+       
+        <div class="hidden your-order" id="order">
+        <h3 class="order-text">Your order</h3>
+        <div class="order-list">
+            <h3 class="order-item">${food.name}s</h3>
+            <h6 class="remove-item" id="remove-item">remove</h6>
+            <h5 class="order-price">${food.price}</h5>
+        </div>
+        <div class="total-price" id="total-price">
+            <h3 class="price-text">Total price:</h3>
+            <h5 class="total-price-text" id="total-price-text">$26</h5>
+        </div>
+        <button class="complete-btn" id="complete-btn">Complete Order</button>
+        
+    </div>
+        
+       
+    `
+    })
+
+    return orderHtml
+    
+}
+
+// shows the items available on the menu 
 function getMenuHtml(){
     let menuHtml = ``
 
